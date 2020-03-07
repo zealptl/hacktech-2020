8489af4d019a16eb5ea56c22005fb1cdb3c419c6
zealptl <zealpatel06@gmail.com>
2020-03-07 15:29:43 -0500

    added markup and styling for forms and tips

--- corona-webapp/app.js
+++ corona-webapp/app.js
@@ -1,53 +1,75 @@
 // Your web app's Firebase configuration
-// var firebaseConfig = {
-//   apiKey: "AIzaSyC0ypF7SnpAn-hCs1SIct_56ZNQQmI16SY",
-//   authDomain: "coronaracism.firebaseapp.com",
-//   databaseURL: "https://coronaracism.firebaseio.com",
-//   projectId: "coronaracism",
-//   storageBucket: "coronaracism.appspot.com",
-//   messagingSenderId: "103969106918",
-//   appId: "1:103969106918:web:e64039fa64087bd98e8427",
-//   measurementId: "G-6X7NRFQTYN"
-// };
+var firebaseConfig = {
+  apiKey: "AIzaSyC0ypF7SnpAn-hCs1SIct_56ZNQQmI16SY",
+  authDomain: "coronaracism.firebaseapp.com",
+  databaseURL: "https://coronaracism.firebaseio.com",
+  projectId: "coronaracism",
+  storageBucket: "coronaracism.appspot.com",
+  messagingSenderId: "103969106918",
+  appId: "1:103969106918:web:e64039fa64087bd98e8427",
+  measurementId: "G-6X7NRFQTYN"
+};
 
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// // Firebase locations database connection set up
-// const dbRef = firebase.database().ref();
-// const locationsRef = dbRef.child("locations");
+// Firebase locations database connection set up
+const dbRef = firebase.database().ref();
+const locationsRef = dbRef.child("locations");
 
-// // Populate rows in list
-// const locationListUI = document.getElementById("locationList");
-// locationsRef.on("child_added", snap => {
-//   let location = snap.val();
-//   let $li = document.createElement("li");
-//   $li.innerHTML = location.name;
-//   $li.setAttribute("child-key", snap.key);
-//   $li.addEventListener("click", locationClicked);
-//   locationListUI.append($li);
-// });
+// Populate rows in list
+const locationListUI = document.getElementById("locationList");
+locationsRef.on("child_added", snap => {
+  let location = snap.val();
+  let $li = document.createElement("li");
+  $li.innerHTML = location.name;
+  $li.setAttribute("child-key", snap.key);
+  $li.addEventListener("click", locationClicked);
+  locationListUI.append($li);
+});
 
-// // Show detail when clicked
-// function locationClicked(e) {
-//   var locationID = e.target.getAttribute("child-key");
-//   const locationRef = dbRef.child("locations/" + locationID);
-//   const locationDetailUI = document.getElementById("locationDetail");
-//   locationDetailUI.innerHTML = "";
-//   locationRef.on("child_added", snap => {
-//     var $p = document.createElement("p");
-//     $p.innerHTML = snap.key + " - " + snap.val();
-//     locationDetailUI.append($p);
-//   });
-// }
+// Show detail when clicked
+function locationClicked(e) {
+  var locationID = e.target.getAttribute("child-key");
+  const locationRef = dbRef.child("locations/" + locationID);
+  const locationDetailUI = document.getElementById("locationDetail");
+  locationDetailUI.innerHTML = "";
+  locationRef.on("child_added", snap => {
+    var $p = document.createElement("p");
+    $p.innerHTML = snap.key + " - " + snap.val();
+    locationDetailUI.append($p);
+  });
+}
 
 // Google Maps
-var map;
 function initMap() {
   var options = {
     zoom: 8,
     center: { lat: 40.7831, lng: -73.9712 }
   };
 
-  var map = new google.maps.Map(document.getElementById("map", options));
+  var map = new google.maps.Map(document.getElementById("map"), options);
+
+  var markers = [
+    {
+      coords: { lat: 40.7831, lng: -73.9712 }
+    },
+    {
+      coords: { lat: 40.742054, lng: -73.769417 }
+    },
+    {
+      coords: { lat: 40.650002, lng: -73.949997 }
+    }
+  ];
+
+  for (let i = 0; i < markers.length; i++) {
+    addMarkers(markers[i]);
+  }
+
+  function addMarkers(props) {
+    var marker = new google.maps.Marker({
+      position: props.coords,
+      map: map
+    });
+  }
 }
